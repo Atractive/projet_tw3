342b353bf8ac936c31e78193c12945766d0d4579
Diegoortizz <ortiz.diego81@gmail.com>
2019-03-27 03:36:37 +0100

    création d'un serveur node js, création d'une carte avec les données geojson, début de clustermakers

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+const http = require('http');
+var path = require('path');
+let geojson = require('./data.json');
+
+
+app.use(express.static('./'));
+
+app.get('/geojson', function (request, response) {
+    console.log("in geojson")
+    response.send(geojson);
+});
+
+app.get('/clusterexample', function (request, response) {
+    response.sendFile(path.join(__dirname, './', 'clusterexample.html'));
+});
+
+app.get('/index', function (request, response) {
+    response.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+http.createServer(app).listen(444, function () {
+    console.log("Server is up and running...");
+});
+
+
+
+// app.get('/', function (request, response) {
+//     response.sendfile('./index.html');
+// });
