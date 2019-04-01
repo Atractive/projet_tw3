445f397ccd24551d801de28936d3068db9531532
Diegoortizz <ortiz.diego81@gmail.com>
2019-04-01 22:45:28 +0200

    ajout de la base de donnée, changement du fond de carte

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ var express = require('express');
 var app = express();
 const http = require('http');
 var path = require('path');
-let geojson = require('./data_full.json');
-// let geojson = require('./data.json');
+const Datastore = require('nedb');
 
+// let geojson = require('./data_full.json');
+let geojson = require('./data.json');
+
+var db = new Datastore({ filename: 'data.db', autoload: true, corruptAlertThreshold: 1 });
+
+// for (var i = 0; i < geojson.length; i++){
+//     db.insert(geojson[i]);
+// }
 
 app.use(express.static('./'));
 
 app.get('/geojson', function (request, response) {
-    console.log("in geojson")
-    response.send(geojson);
+    db.find({}, function (err, docs) {
+        response.send(geojson);
+    });
+
 });
+
+
+// app.get('/geojson', function (request, response) {
+//     console.log("in geojson")
+//     response.send(geojson);
+// });
 
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname, './', 'map.html'));
